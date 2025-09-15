bc6157cb6c600cd13ff5b337c4e9da2ba087ea12
AndomiXD <vintin4some@gmail.com>
2025-09-15 09:48:30 +0300

    random word gen and displayer

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ console.log("script is on")
 const wordsLength = words.length
 console.log(words)
 const gameDuration = 60 * 1000 //60 milliseconds * 1000 = 60 seconds
+
+//generate random words and their random index, -1 because the location/index always starts at 0
+const randomWord = () => {
+  const randIndex = Math.ceil(Math.random() * wordsLength)
+  return words[randIndex - 1]
+}
+
+//inject the randomly generated word to the html and slice them into individual letters with no spaces between them
+const displayWord = (word) => {
+  return `<div class="word"><span class="letter">${word
+    .split("")
+    .join('</span><span class="letter">')}</span></div>`
+}
+console.log(randomWord())
+console.log(displayWord())
