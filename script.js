c4c31a0d30f90b276b861cca9a68dd9b75820e97
AndomiXD <vintin4some@gmail.com>
2025-09-15 09:42:54 +0300

    added words and variables

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-alert("THERE'S A RED PARAGRAPH!!!")
+console.log("script is on")
+//variables
+const wordsLength = words.length
+console.log(words)
+const gameDuration = 60 * 1000 //60 milliseconds * 1000 = 60 seconds
