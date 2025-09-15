8ba79b6a6d577e2621650585f20b89d7f1da7617
AndomiXD <vintin4some@gmail.com>
2025-09-15 09:57:57 +0300

    newgame function and class creation

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//-----------------------------------------------------
 console.log("script is on")
 //variables
 const wordsLength = words.length
@@ -6,8 +7,8 @@ const gameDuration = 60 * 1000 //60 milliseconds * 1000 = 60 seconds
 
 //generate random words and their random index, -1 because the location/index always starts at 0
 const randomWord = () => {
-  const randIndex = Math.ceil(Math.random() * wordsLength)
-  return words[randIndex - 1]
+  const randomIndex = Math.ceil(Math.random() * wordsLength)
+  return words[randomIndex - 1]
 }
 
 //inject the randomly generated word to the html and slice them into individual letters with no spaces between them
@@ -16,5 +17,23 @@ const displayWord = (word) => {
     .split("")
     .join('</span><span class="letter">')}</span></div>`
 }
-console.log(randomWord())
-console.log(displayWord())
+
+//new class creating function
+const addClass = (element, name) => {
+  element.className = element.className + " " + name
+}
+
+//initialise a new game by adding the randomly generated words to the HTML
+const newGame = () => {
+  document.getElementById("words").innerHTML = ""
+  for (let i = 0; i < 100; i++) {
+    // number of words to be displayed is controlled by i < n
+    document.getElementById("words").innerHTML += displayWord(randomWord())
+  }
+  addClass(document.querySelector(".word"), "latest")
+  addClass(document.querySelector(".letter"), "latest")
+  document.getElementById("duration").innerHTML =
+    "Timer: " + gameDuration / 1000 + "s"
+}
+
+console.log(newGame())
